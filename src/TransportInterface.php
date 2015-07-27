32d0ec4c4a04035f97c4b507c62c563712e0412e
Daniel Kreuer <d.kreuer@danielkreuer.com>
2015-07-27 07:54:54 +0200

    Add method to build query string

--- src/TransportInterface.php
+++ src/TransportInterface.php
@@ -23,9 +23,13 @@ interface TransportInterface
     public function newRequest();
 
     /**
-     * Build query string as required for PSR 7 URI ->withQuery
+     * Build a query string from an array of key value pairs
      *
+     * @param array     $params   Query string parameters.
+     * @param int|false $encoding Set to false to not encode, PHP_QUERY_RFC3986
+     *                            to encode using RFC3986, or PHP_QUERY_RFC1738
+     *                            to encode using RFC1738.
      * @return string
      */
-    public function buildQuery();
+    public function buildQuery(array $params, $encoding = PHP_QUERY_RFC3986);
 }
